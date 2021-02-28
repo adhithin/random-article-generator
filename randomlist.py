152daee30c8886284b502704ed000fbb669ea59e
adhithin <adhithi.nmurthy07@gmail.com>
2021-02-27 17:41:45 -0800

    added randomlist - start off with random color

---
+++ randomlist.py
@@ -0,0 +1,7 @@
+import random
+
+list1 = ["red","green", "blue" ]
+
+color = random.choice(list1)
+
+print(color)
--- scraper.py
+++ scraper.py
@@ -1,27 +1,40 @@
+import os
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from flask import request
 import requests
 from bs4 import BeautifulSoup
 import random
 
-response = requests.get(
-    url="https://en.wikipedia.org/wiki/Web_scraping",
-)
-soup = BeautifulSoup(response.content, 'html.parser')
+app = Flask(__name__)
 
-title = soup.find(id="firstHeading")
-print(title.content)
+@app.route('/')
+def scrapeWikiArticle(url):
+    response = requests.get(
+        url=url,
+    )
 
-# Get all the links
-allLinks = soup.find(id="bodyContent").find_all("a")
-random.shuffle(allLinks)
-linkToScrape = 0
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-for link in allLinks:
-    # We are only interested in other wiki articles
-    if link['href'].find("/wiki/") == -1:
-        continue
+    title = soup.find(id="firstHeading")
+    print(title.text)
 
-    # Use this link to scrape
-    linkToScrape = link
-    break
+    allLinks = soup.find(id="bodyContent").find_all("a")
+    random.shuffle(allLinks)
+    linkToScrape = 0
 
-print(linkToScrape)
+    for link in allLinks:
+        # We are only interested in other wiki articles
+        if link['href'].find("/wiki/") == -1:
+            continue
+
+        # Use this link to scrape
+        linkToScrape = link
+        break
+
+    scrapeWikiArticle("https://en.wikipedia.org" + linkToScrape['href'])
+
+scrapeWikiArticle("https://en.wikipedia.org/wiki/Web_scraping")
+
+if __name__ == "__main__":
+    #runs the application on the repl development server
+    app.run(debug=True, port='8292', host='127.0.0.1')
